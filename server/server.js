271369e1db6b3316a6d575ab9c8e49009d9aa597
Marek <marekz@gmail.com>
2013-08-17 17:38:56 -0400

    Add server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+var resources = {};
+
+resources['8FABC4'] = "http://crasstalk.com/wp-content/uploads/2012/03/funny-gifs-this-cat-is-hyped.gif";
+resources['8FABC4'] = "http://crasstalk.com/wp-content/uploads/2012/03/funny-gifs-this-cat-is-hyped.gif";
+resources['8FABC4'] = "http://crasstalk.com/wp-content/uploads/2012/03/funny-gifs-this-cat-is-hyped.gif";
+
+
+app.get('/get', function(req, res){
+  var resource = resources[req.query.id];
+
+  if (!resource) return res.send("Not Found");
+
+  res.send(resource);
+
+});
+
+app.get('/set', function(req, res) {
+	if (req.query.id == null && req.query.value == null) {
+		
+		return res.send(req.query);
+	}
+	resources[req.query.id] = req.query.value;
+	res.send(req.query.value)
+});
+
+
+app.get('/all', function(req, res) {
+	res.send(resources);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on', port);
