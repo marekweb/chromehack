812e6d0978e790f3d92845db5cd580083e1a49a4
Marek <marekz@gmail.com>
2013-08-17 10:34:29 -0400

    Initial commit with webcam test

---
+++ getusermedia.js
@@ -0,0 +1,43 @@
+
+var UPDATE_DELAY = 500;
+var HEIGHT = 480;
+var WIDTH = 640;
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+$(document).ready(ready);
+
+function ready() {
+	navigator.webkitGetUserMedia({video: true}, getUserMediaSuccess, getUserMediaError);
+}
+
+function getUserMediaError(e) {
+	console.error(e);
+}
+
+var videoElem, canvasElem;
+
+
+function getUserMediaSuccess(localMediaStream) {
+
+	$('#prompt').hide();
+
+	// Get element
+	videoElem = document.getElementById('video');
+	canvasElem = document.getElementById('canvas');
+
+	videoElem.src = window.URL.createObjectURL(localMediaStream);
+
+	video.play();
+
+	setInterval(update, UPDATE_DELAY);
+
+}
+
+
+function update() {
+	var ctx = canvasElem.getContext('2d');
+
+	ctx.drawImage(videoElem, 0, 0);
+
+}
